ec20355d1f5399d0a873645c59475716cfd3cfec
konstantine168 <basapapava@gmail.com>
2024-12-06 18:30:18 +0400

    commit

---
+++ level 46/classwork/classwork.py
@@ -0,0 +1,11 @@
+import random # <-- import-ს შეუძლია წამოიღოს წიგნები ბიბლიოთეკიდან :)
+
+mylist = [
+    "Luka",
+    "Sandro",
+    "Dato",
+    "Nino",
+    "Salome",
+    "Nika",
+]
+print(random.choice(mylist)) 
---
+++ level 46/homework/homework.1.py
@@ -0,0 +1,11 @@
+import random
+# 1)
+def animal_random(animal_lst):
+    return random.append.choise(animal_lst)
+# 2)
+def film_random(film_list_item_1, film_list_item_2, film_list_item_3, film_list_item_4):
+    return random.append.choise(film_list_item_1)
+    return random.append.choise(film_list_item_2)
+    return random.append.choise(film_list_item_3)
+    return random.append.choise(film_list_item_4)
+# 3)
