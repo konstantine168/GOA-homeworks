492f160b85c1e023e5b1c4d6b10451f62ad0a8cd
konstantine168 <basapapava@gmail.com>
2025-09-02 17:45:11 +0400

    commit

---
+++ level 122/homework/homework.js
@@ -0,0 +1,22 @@
+// 1
+let list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+while (i < 20) {
+    console.log(i++; i += 3)
+}
+// 2
+let person = {
+  name: "Konti",
+  age: 10,
+};
+const keys = Object.keys(person)
+// 3
+let arr = ["Nino", "Giorgi", "Tamar"]
+let newArr = new Map ([
+    ['Hello', 'Nino']
+    ['Hello', 'Giorgi']
+    ['Hello', 'Tamar']
+])
+// 4 ???
+// 5
+arr [1, 3, 8, 12, 17]
+const filtered = numbers.filter(num > 10);
---
+++ level 124/homework/homework.js
@@ -0,0 +1,7 @@
+// hw 1
+class BookClass{
+    constructor(name, author) {
+        this.name = name
+        this.author = author
+    }
+}
